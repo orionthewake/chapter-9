aebc8aa669be765ae695fbf699c1df4c47ba37de
Joseph Howard <joe.howard@mac.com>
2024-11-04 10:23:06 -0500

    Refactor code for consistency and readability across multiple files

--- example-4/db-fixture-rest-api/src/index.js
+++ example-4/db-fixture-rest-api/src/index.js
@@ -2,13 +2,13 @@
 
 const inProduction = process.env.NODE_ENV === "production";
 if (inProduction) {
-    throw new Error("Don't run DB FIXTURE API in production!!");
+  throw new Error("Don't run DB FIXTURE API in production!!");
 }
 
-const express = require('express');
-const Fixtures = require('node-mongodb-fixtures');
-const path = require('path');
-const{ MongoClient } = require('mongodb');
+const express = require("express");
+const Fixtures = require("node-mongodb-fixtures");
+const path = require("path");
+const { MongoClient } = require("mongodb");
 const globby = require("globby");
 
 const app = express();
@@ -22,63 +22,77 @@ console.log("Using DBHOST " + databaseHost);
 // Connect to the database.
 //
 async function connectDatabase() {
-    return await MongoClient.connect(databaseHost, { useNewUrlParser: true });
+  return await MongoClient.connect(databaseHost, { useNewUrlParser: true });
 }
 
 //
 // Start the HTTP server.
 //
 function startServer() {
-    return new Promise((resolve, reject) => {
-        var server = app.listen(port, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    var server = app.listen(port, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-            const addrInfo = server.address();
-            const host = addrInfo.address;
-            const port = addrInfo.port;
-            console.log("DB fixture REST API listening at http://%s:%s", host, port);
-            console.log("Please put your database fixtures in the 'fixtures' sub-directory.")
-            console.log("Use the following endpoints to load and unload your database fixtures:");
-            console.log(`HTTP GET http://localhost:${port}/load-fixture?db=<db-name>&fix=<your-fixture-name>`);
-            console.log(`HTTP GET http://localhost:${port}/unload-fixture?db=<db-name>&fix=<your-fixture-name>`);
-            console.log(`HTTP GET http://localhost:${port}/drop-collection?db=<db-name>&col=<collection-name>`);
-            console.log(`HTTP GET http://localhost:${port}/drop-database?db=<db-name>`);
-            console.log(`HTTP GET http://localhost:${port}/get-collection?db=<db-name>&col=<collection-name>`);
-			resolve(server);
-        });
+      const addrInfo = server.address();
+      const host = addrInfo.address;
+      const port = addrInfo.port;
+      console.log("DB fixture REST API listening at http://%s:%s", host, port);
+      console.log(
+        "Please put your database fixtures in the 'fixtures' sub-directory."
+      );
+      console.log(
+        "Use the following endpoints to load and unload your database fixtures:"
+      );
+      console.log(
+        `HTTP GET http://localhost:${port}/load-fixture?db=<db-name>&fix=<your-fixture-name>`
+      );
+      console.log(
+        `HTTP GET http://localhost:${port}/unload-fixture?db=<db-name>&fix=<your-fixture-name>`
+      );
+      console.log(
+        `HTTP GET http://localhost:${port}/drop-collection?db=<db-name>&col=<collection-name>`
+      );
+      console.log(
+        `HTTP GET http://localhost:${port}/drop-database?db=<db-name>`
+      );
+      console.log(
+        `HTTP GET http://localhost:${port}/get-collection?db=<db-name>&col=<collection-name>`
+      );
+      resolve(server);
     });
+  });
 }
 
 //
 // Load a fixture to the database.
 //
-async function loadFixture (databaseName, fixtureName) {
-    const fixtures = new Fixtures({
-        dir: path.join(fixturesDirectory, fixtureName),
-        mute: false,
-    });
+async function loadFixture(databaseName, fixtureName) {
+  const fixtures = new Fixtures({
+    dir: path.join(fixturesDirectory, fixtureName),
+    mute: false,
+  });
 
-    await fixtures.connect(databaseHost + "/" + databaseName);
-    await fixtures.unload();
-    await fixtures.load();
-    await fixtures.disconnect();
+  await fixtures.connect(databaseHost + "/" + databaseName);
+  await fixtures.unload();
+  await fixtures.load();
+  await fixtures.disconnect();
 }
 
 //
 // Unload a fixture from the database.
 //
 async function unloadFixture(databaseName, fixtureName) {
-    const fixtures = new Fixtures({
-        dir: path.join(fixturesDirectory, fixtureName),
-        mute: false,
-    });
+  const fixtures = new Fixtures({
+    dir: path.join(fixturesDirectory, fixtureName),
+    mute: false,
+  });
 
-    await fixtures.connect(databaseHost + "/" + databaseName);
-    await fixtures.unload();
-    await fixtures.disconnect();
+  await fixtures.connect(databaseHost + "/" + databaseName);
+  await fixtures.unload();
+  await fixtures.disconnect();
 }
 
 //
@@ -87,166 +101,238 @@ async function unloadFixture(databaseName, fixtureName) {
 // Source: https://stackoverflow.com/questions/21023982/how-to-check-if-a-collection-exists-in-mongodb-native-nodejs-driver
 //
 async function collectionExists(client, databaseName, collectionName) {
-    const db = client.db(databaseName);
-    const collectionNames = await db.listCollections().toArray();
-    return collectionNames.some(collection => collection.name === collectionName);
+  const db = client.db(databaseName);
+  const collectionNames = await db.listCollections().toArray();
+  return collectionNames.some(
+    (collection) => collection.name === collectionName
+  );
 }
 
 //
 // Drop a collection if it exists.
 //
 async function dropCollection(client, databaseName, collectionName) {
-    const collectionAlreadyExists = await collectionExists(client, databaseName, collectionName);
-    if (collectionAlreadyExists) {
-        const db = client.db(databaseName);
-        await db.dropCollection(collectionName);
-        console.log("Dropped collection: " + collectionName);
-    }
-    else {
-        console.log("Collection doesn't exist: " + collectionName);
-    }
+  const collectionAlreadyExists = await collectionExists(
+    client,
+    databaseName,
+    collectionName
+  );
+  if (collectionAlreadyExists) {
+    const db = client.db(databaseName);
+    await db.dropCollection(collectionName);
+    console.log("Dropped collection: " + collectionName);
+  } else {
+    console.log("Collection doesn't exist: " + collectionName);
+  }
 }
 
 async function main() {
+  const client = await connectDatabase();
 
-    const client = await connectDatabase();
-
-    app.get("/is-alive", (req, res) => {
-        res.json({
-            ok: true,
-        });
+  app.get("/is-alive", (req, res) => {
+    res.json({
+      ok: true,
     });
+  });
 
-    function verifyQueryParam(req, res, paramName, msg) {
-        const param = req.query[paramName];
-        if (!param) {
-            res.status(400).send(msg);
-        }
-        return param;
+  function verifyQueryParam(req, res, paramName, msg) {
+    const param = req.query[paramName];
+    if (!param) {
+      res.status(400).send(msg);
+    }
+    return param;
+  }
+
+  app.get("/load-fixture", (req, res) => {
+    const databaseName = verifyQueryParam(
+      req,
+      res,
+      "db",
+      "Query parameter 'db' specifies database name."
+    );
+    const fixtureName = verifyQueryParam(
+      req,
+      res,
+      "fix",
+      "Query parameter 'fix' specifies name of fixture to load into database."
+    );
+    if (!databaseName || !fixtureName) {
+      return;
     }
 
-    app.get("/load-fixture", (req, res) => {
-        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
-        const fixtureName = verifyQueryParam(req, res, "fix", "Query parameter 'fix' specifies name of fixture to load into database."); 
-        if (!databaseName || !fixtureName) {
-            return;
-        }
+    loadFixture(databaseName, fixtureName)
+      .then(() => {
+        console.log(
+          "Loaded database fixture: " +
+            fixtureName +
+            " to database " +
+            databaseName
+        );
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        const msg =
+          "Failed to load database fixture " +
+          fixtureName +
+          " to database " +
+          databaseName;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(400).send(msg);
+      });
+  });
 
-        loadFixture(databaseName, fixtureName)
-            .then(() => {
-                console.log("Loaded database fixture: " + fixtureName + " to database " + databaseName);
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                const msg = "Failed to load database fixture " + fixtureName + " to database " + databaseName;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(400).send(msg);
-            });
-    });
+  app.get("/unload-fixture", (req, res) => {
+    const databaseName = verifyQueryParam(
+      req,
+      res,
+      "db",
+      "Query parameter 'db' specifies database name."
+    );
+    const fixtureName = verifyQueryParam(
+      req,
+      res,
+      "fix",
+      "Query parameter 'fix' specifies name of fixture to load into database."
+    );
+    if (!databaseName || !fixtureName) {
+      return;
+    }
 
-    app.get("/unload-fixture", (req, res) => {
-        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
-        const fixtureName = verifyQueryParam(req, res, "fix", "Query parameter 'fix' specifies name of fixture to load into database."); 
-        if (!databaseName || !fixtureName) {
-            return;
-        }
+    unloadFixture(databaseName, fixtureName)
+      .then(() => {
+        console.log(
+          "Unloaded database fixture: " +
+            fixtureName +
+            " from database " +
+            databaseName
+        );
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        const msg =
+          "Failed to unload database fixture " +
+          fixtureName +
+          " from database " +
+          databaseName;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(400).send(msg);
+      });
+  });
 
-        unloadFixture(databaseName, fixtureName)
-            .then(() => {
-                console.log("Unloaded database fixture: " + fixtureName + " from database " + databaseName);
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                const msg = "Failed to unload database fixture " + fixtureName + " from database " + databaseName;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(400).send(msg);
-            });
-    });
-    
-    app.get("/drop-collection", (req, res) => {
-        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
-        const collectionName = verifyQueryParam(req, res, "col", "Query parameter 'col' specifies name of collection to drop."); 
-        if (!databaseName || !collectionName) {
-            return;
-        }
+  app.get("/drop-collection", (req, res) => {
+    const databaseName = verifyQueryParam(
+      req,
+      res,
+      "db",
+      "Query parameter 'db' specifies database name."
+    );
+    const collectionName = verifyQueryParam(
+      req,
+      res,
+      "col",
+      "Query parameter 'col' specifies name of collection to drop."
+    );
+    if (!databaseName || !collectionName) {
+      return;
+    }
 
-        dropCollection(client, databaseName, collectionName)
-            .then(() => {
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                const msg = "Failed to drop collection " + collectionName + " from database " + databaseName;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(400).send(msg);
-            });
-    });
+    dropCollection(client, databaseName, collectionName)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        const msg =
+          "Failed to drop collection " +
+          collectionName +
+          " from database " +
+          databaseName;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(400).send(msg);
+      });
+  });
 
-    app.get("/drop-database", (req, res) => {
-        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
-        if (!databaseName) {
-            return;
-        }
+  app.get("/drop-database", (req, res) => {
+    const databaseName = verifyQueryParam(
+      req,
+      res,
+      "db",
+      "Query parameter 'db' specifies database name."
+    );
+    if (!databaseName) {
+      return;
+    }
 
-        dropDatabase(client, databaseName, collectionName)
-            .then(() => {
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                const msg = "Failed to drop database " + databaseName;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(400).send(msg);
-            });
-    });
+    dropDatabase(client, databaseName, collectionName)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        const msg = "Failed to drop database " + databaseName;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(400).send(msg);
+      });
+  });
 
-    app.get("/get-collection", (req, res) => {
-        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
-        const collectionName = verifyQueryParam(req, res, "col", "Query parameter 'col' specifies name of collection to drop."); 
-        if (!databaseName || !collectionName) {
-            return;
-        }
+  app.get("/get-collection", (req, res) => {
+    const databaseName = verifyQueryParam(
+      req,
+      res,
+      "db",
+      "Query parameter 'db' specifies database name."
+    );
+    const collectionName = verifyQueryParam(
+      req,
+      res,
+      "col",
+      "Query parameter 'col' specifies name of collection to drop."
+    );
+    if (!databaseName || !collectionName) {
+      return;
+    }
 
-        const db = client.db(databaseName);
-        db.collection(collectionName) //TODO: helper function?
-            .find()
-            .toArray()
-            .then(documents => {
-                res.json(documents);
-            })
-            .catch(err => {
-                const msg = "Failed to get collection " + collectionName + " from database " + databaseName;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(400).send(msg);
-            });
-    });
-    
-    app.get("/get-fixtures", (req, res) => {
+    const db = client.db(databaseName);
+    db.collection(collectionName) //TODO: helper function?
+      .find()
+      .toArray()
+      .then((documents) => {
+        res.json(documents);
+      })
+      .catch((err) => {
+        const msg =
+          "Failed to get collection " +
+          collectionName +
+          " from database " +
+          databaseName;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(400).send(msg);
+      });
+  });
 
-        globby([fixturesDirectory + "/**/*.js", fixturesDirectory + "/**/*.json"])
-            .then(fixtureFilePaths => {
+  app.get("/get-fixtures", (req, res) => {
+    globby([fixturesDirectory + "/**/*.js", fixturesDirectory + "/**/*.json"])
+      .then((fixtureFilePaths) => {
+        const fixtureNames = fixtureFilePaths.map((fixtureFilePath) =>
+          path.basename(path.dirname(fixtureFilePath))
+        );
+        res.json(fixtureNames);
+      })
+      .catch((err) => {
+        const msg = "Failed to list fixtures in directory" + fixturesDirectory;
+        console.error(msg);
+        console.error((err && err.stack) || err);
+        res.status(500).send(msg);
+      });
+  });
 
-                const fixtureNames = fixtureFilePaths.map(fixtureFilePath => path.basename(path.dirname(fixtureFilePath)));
-                res.json(fixtureNames);
-            })
-            .catch(err => {
-                const msg = "Failed to list fixtures in directory" + fixturesDirectory;
-                console.error(msg);
-                console.error(err && err.stack || err);
-                res.status(500).send(msg);
-            });
-    });
-
-    await startServer();
+  await startServer();
 }
 
-main()
-    .catch(err => {
-        console.error("DB fixture REST API failed to start.");
-        console.error(err && err.stack || err);
-    });
-
-
+main().catch((err) => {
+  console.error("DB fixture REST API failed to start.");
+  console.error((err && err.stack) || err);
+});
--- example-4/gateway/src/index.js
+++ example-4/gateway/src/index.js
@@ -3,7 +3,9 @@ const path = require("path");
 const axios = require("axios");
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
 }
 
 const PORT = process.env.PORT;
@@ -12,42 +14,39 @@ const PORT = process.env.PORT;
 // Starts the microservice.
 //
 async function startMicroservice(dbhost, dbname) {
+  const app = express();
 
-    const app = express();
+  app.set("views", path.join(__dirname, "views")); // Set directory that contains templates for views.
+  app.set("view engine", "hbs"); // Use hbs as the view engine for Express.
 
-    app.set("views", path.join(__dirname, "views")); // Set directory that contains templates for views.
-    app.set("view engine", "hbs"); // Use hbs as the view engine for Express.
-    
-    app.use(express.static("public"));
+  app.use(express.static("public"));
 
-    //
-    // Main web page that lists videos.
-    //
-    app.get("/", async (req, res) => {
+  //
+  // Main web page that lists videos.
+  //
+  app.get("/", async (req, res) => {
+    // Retreives the list of videos from the metadata microservice.
+    const videosResponse = await axios.get("http://metadata/videos");
 
-        // Retreives the list of videos from the metadata microservice.
-        const videosResponse = await axios.get("http://metadata/videos");
+    // Renders the video list for display in the browser.
+    res.render("video-list", { videos: videosResponse.data.videos });
+  });
 
-        // Renders the video list for display in the browser.
-        res.render("video-list", { videos: videosResponse.data.videos });
-    });
-    
-    // Add other route handlers here.
+  // Add other route handlers here.
 
-	app.listen(PORT, () => {
-        console.log("Microservice online.");
-    });    
+  app.listen(PORT, () => {
+    console.log("Microservice online.");
+  });
 }
 
 //
 // Application entry point.
 //
 async function main() {
-    await startMicroservice();
+  await startMicroservice();
 }
 
-main()
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
